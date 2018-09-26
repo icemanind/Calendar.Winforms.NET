9f0538a31e81f10ef0a4cd43d9c8b02f0e646f98
Alan Bryan <icemanind9@gmail.com>
2018-09-26 13:51:08 -0400

    Added Some Calendar Properties

--- Calendar.Demo/MainForm.Designer.cs
+++ Calendar.Demo/MainForm.Designer.cs
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.calendar1 = new Calendar.UserControl.Calendar();
             this.SuspendLayout();
+            // 
+            // calendar1
+            // 
+            this.calendar1.BackgroundColor = System.Drawing.Color.Transparent;
+            this.calendar1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.calendar1.CalendarEventsFont = new System.Drawing.Font("Arial", 12F);
+            this.calendar1.CalendarFont = new System.Drawing.Font("Arial", 12F);
+            this.calendar1.CalendarSizeMode = Calendar.UserControl.CalendarSizeModes.Stretch;
+            this.calendar1.Location = new System.Drawing.Point(246, 78);
+            this.calendar1.Name = "calendar1";
+            this.calendar1.ShowMonthYear = true;
+            this.calendar1.ShowNextMonthButton = true;
+            this.calendar1.ShowPreviousMonthButton = true;
+            this.calendar1.ShowTodayButton = true;
+            this.calendar1.Size = new System.Drawing.Size(849, 571);
+            this.calendar1.TabIndex = 0;
+            this.calendar1.TodayButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.calendar1.TodayButtonFont = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold);
+            this.calendar1.TodayButtonTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1379, 825);
+            this.Controls.Add(this.calendar1);
             this.Name = "MainForm";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +63,8 @@
         }
 
         #endregion
+
+        private UserControl.Calendar calendar1;
     }
 }
 
--- Calendar.UserControl/Calendar.cs
+++ Calendar.UserControl/Calendar.cs
@@ -13,21 +13,61 @@ namespace Calendar.UserControl
     {
         public bool ShowTodayButton { get; set; }
         public Font TodayButtonFont { get; set; }
+        public Color TodayButtonColor { get; set; }
+        public Color TodayButtonTextColor { get; set; }
         public bool ShowPreviousMonthButton { get; set; }
         public bool ShowNextMonthButton { get; set; }
         public bool ShowMonthYear { get; set; }
         public Font CalendarEventsFont { get; set; }
         public Font CalendarFont { get; set; }
+        public Color BackgroundColor { get; set; }
+        public Color BorderColor { get; set; }
+        public CalendarSizeModes CalendarSizeMode { get; set; }
+
+        private const int TopMargin = 15;
 
         public Calendar()
         {
+            InitializeComponent();
+
             ShowTodayButton = true;
             ShowPreviousMonthButton = true;
             ShowNextMonthButton = true;
             ShowMonthYear = true;
             TodayButtonFont = new Font("Arial", 24, FontStyle.Bold);
+            TodayButtonColor = Color.FromArgb(200, 200, 200);
+            TodayButtonTextColor = Color.FromArgb(100, 100, 100);
             CalendarEventsFont = new Font("Arial", 12, FontStyle.Regular);
             CalendarFont = new Font("Arial", 12, FontStyle.Regular);
+            BackgroundColor = Color.Transparent;
+            BorderColor = Color.FromArgb(200, 200, 200);
+            CalendarSizeMode = CalendarSizeModes.Stretch;
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Calendar
+            // 
+            this.DoubleBuffered = true;
+            this.Name = "Calendar";
+            this.Size = new System.Drawing.Size(849, 571);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Calendar_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        private void Calendar_Paint(object sender, PaintEventArgs e)
+        {
+            var bitmap = new Bitmap(ClientSize.Width, ClientSize.Height);
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                g.Clear(BackgroundColor);
+                g.DrawLine(new Pen(Color.Green), 0, 0, 50, 50);
+            }
+
+            e.Graphics.DrawImage(bitmap, 0, 0);
         }
     }
 }
